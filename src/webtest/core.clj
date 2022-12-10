e8cdeb85f565ac54980e4106cad1f6595d4f4e36
Weiqifan1 <cmlykke@hotmail.com>
2022-12-10 14:16:58 +0100

    2022-12-10_1416_postVirker youtubeOnTheCodeAgain)

--- src/webtest/core.clj
+++ src/webtest/core.clj
@@ -5,26 +5,43 @@
             [reitit.ring.middleware.muuntaja :as muuntaja])
   (:gen-class))
 
+(def users (atom {}))
+
 (defn string-handler [_]
   {:status 200
    :body "on the code again"})
+
+(defn create-user [{user :body-params}]
+  (let [id (str (java.util.UUID/randomUUID))
+        users (->> (assoc user :id id)
+                   (swap! users assoc id))]
+    {:status 200
+     :body users}
+    ))
+;(create-user {:body-params {:name "John"}})
+
+(defn get-users [_]
+  @users)
+
+(defn get-user-by-id [{{:keys [id]} :path-params}]
+  {:staus 200
+   :body (get @users id)})
 
 (def app
   (ring/ring-handler
     (ring/router
       ["/"
-       ["" {:get string-handler}]
-       ["math" (:post string-handler)]]
-      {:data {:muuntaja   m/instance
+       ["users/:id" get-user-by-id]
+       ["users" {:get get-users
+                 :post create-user}]
+       ["" string-handler]]
+      {:data {:muuntaja m/instance
               :middleware [muuntaja/format-middleware]}})))
 
+(defn start []
+  (ring-jetty/run-jetty app {:port 3000
+                             :join? false}))
 
-(defn start [] (ring-jetty/run-jetty app {:port  3000
-                                              :join? false}))
-
-(defn -main [& args] (start))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (start))
